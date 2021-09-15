1c4ed9b02c594b7d5262c424419f39e8131e8344
bryantcruz <86982928+bryantcruz@users.noreply.github.com>
2021-09-15 10:16:28 -0700

    added form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,18 +1,31 @@
+var userFormE1 = document.querySelector("#user-form");
+var nameInputE1 = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  console.log(event);
+  //get value from input element
+  var username = nameInputE1.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputE1.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+      console.log(response);
       response.json().then(function(data) {
         console.log(data);
       });
-  console.log("inside", response);
-});
-console.log("outside");
-    var response = fetch("https://api.github.com/users/octocat/repos");
-    console.log(response);
-    console.log("function was called");
-};
+    });
+  };
 
-getUserRepos();
+ userFormE1.addEventListener("submit", formSubmitHandler);

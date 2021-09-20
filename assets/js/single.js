8ac1cf15b05f17ae6b611d9f744a0bb9a709e396
bryantcruz <86982928+bryantcruz@users.noreply.github.com>
2021-09-20 11:42:56 -0700

    error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,8 +1,24 @@
+var repoNameE1 = document.querySelector("#repo-name");
 var issueContainerE1 = document.querySelector("#issues-container");
 var limitWarningE1 = document.querySelector("#limit-warning");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+    
+        getRepoIssues(repoName);
+      } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+      }
+    };
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    console.log(repo);
 
     fetch(apiUrl).then(function(response) {
         // request was successful
@@ -18,7 +34,8 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+          document.location.replace("./index.html");
         }
       });
 };
@@ -72,4 +89,4 @@ var displayWarning = function(repo) {
     // aapend to warning container
     limitWarningE1.appendChild(linkE1);
 }
-getRepoIssues("facebook/react");
+getRepoName();
